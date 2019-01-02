cd0a5aaa5fa1af0a9b706d88b0b613b13a81b1f6
Jennifer DeCota <jdecota@sbcglobal.net>
2019-01-02 13:17:59 -0600

    Access to 3rd view controller ListViewController working;  constraints not in place yet.  ; Data being passed back from VC2 to VC1;

--- CoderGirlFinal/Controllers/FirstViewController.swift
+++ CoderGirlFinal/Controllers/FirstViewController.swift
@@ -2,17 +2,76 @@
 //  CoderGirlFinal
 
 import UIKit
+import Foundation
+import Alamofire
+import SwiftyJSON
+import SVProgressHUD
 
 class FirstViewController: UIViewController, dataToDelegate {
     
+
     @IBOutlet weak var VcOneLabel: UILabel!
     @IBOutlet weak var TextFieldOne: UITextField!
     @IBOutlet weak var ScrollOne: UIScrollView!
     
+    let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    //  let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    var finalURL : String = ""
+    var finalURL1a : String = ""
+    var dataVcOne : String = ""
+    var dataFromVcTwo : String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+    }
+    
+    
+    //MARK: - Networking
+    
+    // func getWeatherData(url: String, parameters: [String : String]) {
+    // -> Edit name and remove parameter;  don't have parameters for bitcoin app
+    func getBitcoinData(url: String) {
+        //Alamofire.request(url, method: .get, parameters: parameters)
+        Alamofire.request(url, method: .get)
+            .responseJSON { response in
+                if response.result.isSuccess {
+                    
+                    print("Successful Connection!")
+                    //format the data we get back
+                    //create constasnt of type JSON with the value of the result of the response
+                    // ( ) to CAST value to JSON Format
+                    //prob 1 comes back in form of optional so need to force unwrapping
+                    //prob 2 is value is
+                    
+                    let bitcoinJSON : JSON = JSON(response.result.value!)
+                    print (bitcoinJSON)
+                    self.updateBitcoinData(json: bitcoinJSON)
+                    
+                } else {
+                    print("Error: \(String(describing: response.result.error))")
+                    self.VcOneLabel.text = "Connection Issues"
+                }
+        }
+    }
+    //MARK: JSON Parsing     passing in the JSON that we got from the api
+    func updateBitcoinData(json : JSON) {
+        
+        // IF LET so we can use Optional Binding
+        // if let bitcoinResult = json["averages"]["day"].double {   // if nested request...
+        if let bitcoinResult = json["ask"].double {
+            // bitcoinResult is a Double so either can use String Interpolation:   = "\(bitcoinResult)"
+            // Or can CAST it into a String:
+            //TextFieldOne.text = String(bitcoinResult)
+            dataVcOne = String(bitcoinResult)
+            VcOneLabel.text = String(bitcoinResult)
+            //* This won't update the label;  need to call this function where we get finalURL
+            // if using currencySymbolArray:   = currencySelected + String(bitcoinResult)
+            
+        } else {
+            
+            TextFieldOne.text = "Information Unavailable"
+        }
     }
     
     //MARK: Segue from button pressed
@@ -20,20 +79,34 @@ class FirstViewController: UIViewController, dataToDelegate {
         
         performSegue(withIdentifier: "sendToSecondVC", sender: self)
     }
-        // Pass the selected object to the new view controller.
-            // Get the new view controller using segue.destination.
+    // Passes the selected object to the new view controller.
+    // Tap into the receiving view controller using segue.destination
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "sendToSecondVC" {
+                        print("prepareForSegue Called")
             let secondSecondVC = segue.destination as! SecondViewController
-            
-            secondSecondVC.data = TextFieldOne.text!
+                        print("secondVC created")
+            finalURL = (baseURL + TextFieldOne.text!)
+                        print(finalURL)
+            getBitcoinData(url: finalURL)
+                        print("getBitcoinData called")
+            // to receive data, must set self as delegate (in prepareForSegue??)
             secondSecondVC.delegate = self
+                //secondSecondVC.data = TextFieldOne.text!
+            secondSecondVC.data = String(dataVcOne)
+                        print("secondsecondVC data equals dataVcOne")
         }
     }
-
+    
     //MARK:  Requirement For Conforming to Delegate
     func dataFromVcTwo(data: String) {
-        VcOneLabel.text = data
+        
+        finalURL1a = String(data)
+        //label.text = data
+        //VcOneLabel.text = data
+        //dataVcOne = data
+        getBitcoinData(url: finalURL1a)
+        
     }
 }
 
--- CoderGirlFinal/Controllers/SecondViewController.swift
+++ CoderGirlFinal/Controllers/SecondViewController.swift
@@ -1,12 +1,12 @@
-//
-//  SecondViewController.swift
-//  CoderGirlFinal
-//
+//  SecondViewController.swift    CoderGirlFinal
 //  Created by Jennifer DeCota on 12/30/18.
 //  Copyright © 2018 iOS Class. All rights reserved.
-//
 
 import UIKit
+import Foundation
+import Alamofire
+import SwiftyJSON
+
 
 protocol dataToDelegate {
     func dataFromVcTwo(data : String)
@@ -19,19 +19,88 @@ class SecondViewController: UIViewController {
     @IBOutlet weak var LabelTwo: UILabel!
     var data = ""
     var delegate : dataToDelegate?
+    //LabelTwo.titleCNY = "placeholder text"
+    var dataToVcOne : String = ""
+    
+    let baseURL2 = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    //  let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    var finalURL2 : String = ""
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         LabelTwo.text = data
+        //LabelTwo.text = "data goes here"
         // Do any additional setup after loading the view.
     }
     
+    //MARK: - Networking
+    
+//    func getBitcoinData(url: String) {
+//        //Alamofire.request(url, method: .get, parameters: parameters)
+//        Alamofire.request(url, method: .get)
+//            .responseJSON { response in
+//                if response.result.isSuccess {
+//
+//                    print("Successful Connection!")
+//                    //format the data we get back
+//                    //create constasnt of type JSON with the value of the result of the response
+//                    // ( ) to CAST value to JSON Format
+//                    //prob 1 comes back in form of optional so need to force unwrapping
+//                    //prob 2 is value is
+//
+//                    let bitcoinJSON : JSON = JSON(response.result.value!)
+//                    print (bitcoinJSON)
+//                    self.updateBitcoinData(json: bitcoinJSON)
+//
+//                } else {
+//                    print("Error: \(String(describing: response.result.error))")
+//                    //self.LabelTwo.text = "Connection Issues"
+//                }
+//        }
+//    }
+//    //MARK: JSON Parsing     passing in the JSON that we got from the api
+//    func updateBitcoinData(json : JSON) {
+//
+//        // IF LET so we can use Optional Binding
+//        // if let bitcoinResult = json["averages"]["day"].double {   // if nested request...
+//        if let bitcoinResult = json["ask"].double {
+//            // bitcoinResult is a Double so either can use String Interpolation:   = "\(bitcoinResult)"
+//            // Or can CAST it into a String:
+//            //TextFieldOne.text = String(bitcoinResult)
+//             dataToVcOne = String(bitcoinResult)
+//            //* This won't update the label;  need to call this function where we get finalURL
+//            // if using currencySymbolArray:   = currencySelected + String(bitcoinResult)
+//
+//        } else {
+//
+//            //TextFieldOne.text = "Information Unavailable"
+//        }
+//    }
+
+    
     @IBAction func VcTwoButtonPressed(_ sender: UIButton) {
         
-        delegate?.dataFromVcTwo(data: TextFieldTwo.text!)
+        print("VcTwoButtonPressed")
+        // Call protcol function property OF delegate PASSING in the data
+        //delegate?.dataFromVcTwo(data: TextFieldTwo.text!)
+    
+//        let finalURL = (baseURL2 + TextFieldTwo.text!)
+//        print(finalURL)
+//        getBitcoinData(url: finalURL)
+        //print("getBitcoinData Called")
+
+        //delegate?.dataFromVcTwo(data: dataToVcOne)
+        //dismiss(animated: true, completion: nil)
+        
+        finalURL2 = (baseURL2 + TextFieldTwo.text!)
+        delegate?.dataFromVcTwo(data: finalURL2)
         dismiss(animated: true, completion: nil)
+        
     }
+    
+
     
     @IBAction func getListButtonPressed(_ sender: UIButton) {
         /* delegate?.dataFromVcTwo(data: TextFieldTwo.text!)
@@ -47,11 +116,8 @@ class SecondViewController: UIViewController {
         
         print("segue.destination equals so data can be entered here" )
         //secondListVc.itemArray =
-
-        dismiss(animated: true, completion: nil)
         }
     }
-        // Mark: NETWORKING
-    
-    
+
+
 }
